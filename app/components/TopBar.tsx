5996c191d0c35f18cbb702955d49490933a8a6f0
Sentharion <michpawbert@gmail.com>
2026-09-12 00:10:14 +0200

    desktop nav and main page

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -1,22 +1,30 @@
+"use client";
 import Ticker from "./Ticker";
-
+import { useEffect } from "react";
 const TopBar = () => {
+  useEffect(() => {
+  console.log("TOPBAR MOUNT");
+
+  return () => {
+    console.log("TOPBAR UNMOUNT");
+  };
+  }, []);
   return (
     <header className="mt-1 bg-[#0a0a14]">
-      <div className="mx-auto flex  max-w-3xl items-center gap-3 px-4 sm:px-22 py-3 sm:max-w-2xl sm:py-2">
+      <div className="mx-auto flex  max-w-3xl items-center gap-3 px-4 sm:px-13 py-3 sm:max-w-2xl sm:py-2">
         
-        <div className="glitch font-russo text-[22px] tracking-wider font-light text-neon-pink glow-pink sm:text-[16px]!">
+        <div className="glitch font-russo text-[22px] tracking-wider font-light text-neon-pink glow-pink sm:text-[24px]!">
           MOONIGHT 
         </div>
 
-        <div className="vhs-badge text-[#555580] text-[10px]! sm:text-[8px]!">
-          <span className="blink text-[#555580]">●</span>{" "}
-          STANDBY
+        <div className="vhs-badge text-text-light text-[10px]! sm:text-[12px]!">
+          <span className="blink text-text-light">●</span>{" "}
+          OCZEKIWANIE
         </div>
 
       </div>
 
-      <Ticker titles={["NO SIGNAL"]} />
+      <Ticker titles={["BRAK SYGNAŁU"]} />
     </header>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,40 @@
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center scanlines">
-      
+    <div className="flex flex-1 flex-col items-center justify-center gap-8 px-6 pt-32 text-center sm:pb-0">
+      <div className="relative">
+        <div className="font-russo select-none text-[80px] leading-none tracking-[0.02em] text-[#1a1a30]">
+          ▶
+        </div>
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="font-russo text-[76px] leading-none text-neon-pink opacity-[0.12] blur-[8px]">
+            ▶
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="font-russo text-[20px] uppercase sm:text-[26px] leading-[1.2] tracking-[0.04em] text-[#e8e0ff]">
+          Brak zaplanowanego
+          <br />
+          <span className="text-neon-pink text-[20px] uppercase sm:text-[26px] [text-shadow:0_0_16px_#ff2d78]">
+            Seansu
+          </span>
+        </div>
+
+        <div className="vhs-badge mt-3 text-[12px] sm:text-[16px] text-text-light">
+          Zacznij nowy seans — zaproś ekipę, wybierz film i ustal datę.
+        </div>
+      </div>
+
+      <button className="rounded-sm border-2 cursor-pointer uppercase hover:scale-115 transition-all duration-100 border-neon-pink bg-neon-pink/[0.07] px-4 py-3 sm:px-8 sm:py-4 font-russo text-[16px] tracking-[0.08em] text-neon-pink shadow-[0_0_24px_#ff2d7840,inset_0_0_20px_#ff2d7808] active:scale-95">
+        ▶ Zaplanuj wieczór filmowy
+      </button>
+
+      <div className="vhs-badge text-[#1e1e38]">
+        CREATED BY SENTHARION
+      </div>
+
     </div>
   );
 }
