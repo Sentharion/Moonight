d39aee1791d3f0c4e8d1c3b9e35744fe9f4b125e
Sentharion <michpawbert@gmail.com>
2026-09-17 15:07:15 +0200

    icons 2

--- app/manifest.ts
+++ app/manifest.ts
@@ -15,18 +15,6 @@ export default function manifest(): MetadataRoute.Manifest {
 
         icons: [
             {
-                src: "/icon-192.png",
-                sizes: "192x192",
-                type: "image/png",
-                purpose: "any"
-            },
-            {
-                src: "/icon-512.png",
-                sizes: "512x512",
-                type: "image/png",
-                purpose: "any"
-            },
-            {
                 src: "/icon-192-maskable.png",
                 sizes: "192x192",
                 type: "image/png",
